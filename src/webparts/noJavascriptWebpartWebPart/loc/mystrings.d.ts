b8e75fbd16a53601c908cd2655c0f70a3a64e8f9
anthony@asmurph.onmicrosoft.com <anthony@asmurph.onmicrosoft.com>
2021-07-28 10:19:04 -0500

    Configured to search Site Pages

---
+++ src/webparts/noJavascriptWebpartWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface INoJavascriptWebpartWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'NoJavascriptWebpartWebPartWebPartStrings' {
+  const strings: INoJavascriptWebpartWebPartWebPartStrings;
+  export = strings;
+}
